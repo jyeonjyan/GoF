fe174dd697f1ade3f37b6e76a56606f39f66b5ae
johnjihwan <s20062@gsm.hs.kr>
2021-11-22 11:06:14 +0900

    Add initial commit

---
+++ src/main/java/com/pattern/GoF/GoFApplication.java
@@ -0,0 +1,13 @@
+package com.pattern.GoF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoFApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pattern/GoF/GoFApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pattern.GoF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
