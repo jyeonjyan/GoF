cfd1e879487427edafbd06aa368eb6fd29663b00
johnjihwan <s20062@gsm.hs.kr>
2021-11-23 09:36:16 +0900

    Add enum class 를 이용한 singleton test

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 ## Task
 * [싱글톤패턴](./src/main/java/com/pattern/GoF/singleton)
 * [싱글톤 패턴 구현 방법을 깨뜨리는 방법](./src/test/java/com/pattern/GoF/singleton/crashSingleton)
+* [싱글톤을 안전하고 단순하게 구현하는 방법](./src/main/java/com/pattern/GoF/singleton/enumType/Settings.java)
+    * [enum을 이용한 싱글톤, 리플랙션 조작 차단](./src/test/java/com/pattern/GoF/singleton/enumType/SettingsTest.java)
+
 
 ## Reference
 * [백기선 - GoF의 디자인 패턴](https://inf.run/kdrm)
--- src/test/java/com/pattern/GoF/singleton/enumType/SettingsTest.java
+++ src/test/java/com/pattern/GoF/singleton/enumType/SettingsTest.java
@@ -1,4 +1,32 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.pattern.GoF.singleton.enumType;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 class SettingsTest {
-  
+    @Test
+    @DisplayName("Enum 으로 안전하게 싱글톤 패턴 구현하기 테스트")
+    void enum_singleton_test() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        // 정상적인 방법으로 생성한 enum class 의 인스턴스
+        Settings instance = Settings.INSTANCE;
+        Settings instance1 = Settings.INSTANCE;
+
+        // enum class 의 인스턴스는 싱글톤 원리에 의해 같다.
+        System.out.println(instance == instance1);
+
+        /**
+         * Cannot reflectively create enum objects 발생 함.
+         * enum은 내부적으로 reflection을 막아둠.
+         *
+         * @see Constructor.java:484
+         */
+        Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
+        for (Constructor<?> constructors : declaredConstructors){
+            constructors.setAccessible(true);
+            Settings newInstance = (Settings) constructors.newInstance();
+        }
+    }
 }
