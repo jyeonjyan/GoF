5c006197aea032aeff432e70f48f8e6336fec7e2
johnjihwan <s20062@gsm.hs.kr>
2021-11-27 21:04:44 +0900

    Update client class 도 factory 하게

--- src/main/java/com/pattern/GoF/factory/applied/Client.java
+++ src/main/java/com/pattern/GoF/factory/applied/Client.java
@@ -1,17 +1,18 @@
 package com.pattern.GoF.factory.applied;
 
+import com.pattern.GoF.factory.applied.car.CarFactory;
 import com.pattern.GoF.factory.applied.car.blueCar.BlueCarFactory;
-import com.pattern.GoF.factory.applied.car.Car;
 import com.pattern.GoF.factory.applied.car.redCar.RedCarFactory;
 
 public class Client {
     public static void main(String[] args) {
         Client client = new Client();
 
-        Car redCar = new RedCarFactory().orderCar("redCar", "jihwan@mail.com");
-        System.out.println(redCar);
+        client.print(new RedCarFactory(), "redCar", "jihwan@mail.com");
+        client.print(new BlueCarFactory(), "blueCar", "jihwan@mail.com");
+    }
 
-        Car blueCar = new BlueCarFactory().orderCar("blueCar", "jihwan@mail.com");
-        System.out.println(blueCar);
+    private void print(CarFactory carFactory, String color, String email) {
+        System.out.println(carFactory.orderCar(color, email));
     }
 }
