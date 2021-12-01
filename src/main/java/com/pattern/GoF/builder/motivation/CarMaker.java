f89c58bc66ff7dfcd19ed1b0bdd136356c2e7f81
johnjihwan <s20062@gsm.hs.kr>
2021-12-01 10:41:07 +0900

    Add builder motivation

--- src/main/java/com/pattern/GoF/builder/motivation/CarMaker.java
+++ src/main/java/com/pattern/GoF/builder/motivation/CarMaker.java
@@ -1,2 +1,28 @@
-package com.pattern.GoF.builder.motivation;public class CarMaker {
+package com.pattern.GoF.builder.motivation;
+
+public class CarMaker {
+    public static void main(String[] args) {
+        /**
+         * Gasoline-based car
+         * engine 이 존재한다.
+         */
+        Car car = new Car();
+        car.setBrand("BMW");
+        car.setEngine("v8");
+        car.setSuspension("m-sports");
+        car.setWheel("m-sports-wheel");
+
+
+        /**
+         * Electric-based car
+         * engine 이 존재하지 않는다.
+         */
+        System.out.println(car);
+        Car electricCar = new Car();
+        electricCar.setBrand("Tesla");
+        electricCar.setSuspension("press");
+        electricCar.setWheel("less-air-resistance");
+
+        // 결론적으로 불안정한 인스턴스를 생성하게 된다.
+    }
 }
