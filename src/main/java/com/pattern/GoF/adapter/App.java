0f6a189ffcb94e4151b4359c29f77bb154ef9bb7
johnjihwan <s20062@gsm.hs.kr>
2022-01-09 20:18:27 +0900

    Add 실행부

--- src/main/java/com/pattern/GoF/adapter/App.java
+++ src/main/java/com/pattern/GoF/adapter/App.java
@@ -1,2 +1,16 @@
-package com.pattern.GoF.adapter;public class App {
+package com.pattern.GoF.adapter;
+
+import com.pattern.GoF.adapter.account.AccountService;
+import com.pattern.GoF.adapter.account.AccountUserDetailsService;
+import com.pattern.GoF.adapter.security.LoginHandler;
+import com.pattern.GoF.adapter.security.UserDetailsService;
+
+public class App {
+    public static void main(String[] args) {
+        AccountService accountService = new AccountService();
+        UserDetailsService userDetailsService = new AccountUserDetailsService(accountService);
+        LoginHandler loginHandler = new LoginHandler(userDetailsService);
+        String jihwan = loginHandler.login("jihwan", "1234");
+        System.out.println(jihwan);
+    }
 }
